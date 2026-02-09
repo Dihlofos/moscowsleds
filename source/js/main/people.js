7d32f2d7734e22da7f2fbc188e4d190ca26800f8
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2026-02-09 08:58:23 +0300

    added stars

--- source/js/main/people.js
+++ source/js/main/people.js
@@ -2,21 +2,21 @@
 (function () {
   const slider = document.querySelector(".js-main-slider-container");
   const vw = window.innerWidth;
-  //const wrapper = slider.querySelector(".swiper-wrapper");
+  const wrapper = slider.querySelector(".swiper-wrapper");
 
-  // new Swiper(`.js-main-slider-concert`, {
-  //   // Optional parameters
-  //   slidesPerView: vw > 767 ? 3 : 1,
-  //   spaceBetween: 40,
-  //   initialSlide: 0,
-  //   draggable: false,
-  //   pagination: false,
-  //   loop: true,
-  //   navigation: {
-  //     nextEl: ".js-main-next-concert",
-  //     prevEl: ".js-main-prev-concert",
-  //   },
-  // });
+  new Swiper(`.js-main-slider-concert`, {
+    // Optional parameters
+    slidesPerView: vw > 767 ? 3 : 1,
+    spaceBetween: 40,
+    initialSlide: 0,
+    draggable: false,
+    pagination: false,
+    loop: true,
+    navigation: {
+      nextEl: ".js-main-next-concert",
+      prevEl: ".js-main-prev-concert",
+    },
+  });
 
   new Swiper(`.js-actions-slider-concert`, {
     // Optional parameters
